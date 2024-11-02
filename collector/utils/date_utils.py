93558ba2a80ac14ab3e3e0d60fdb1b03ff826481
paun93 <jackbauersweet@gmail.com>
2024-11-03 00:07:01 +0100

    default_expiration_date feat: Adjust date_one_year_from_now function to display end of calendar month date that is 365 days from the current date

--- collector/utils/date_utils.py
+++ collector/utils/date_utils.py
@@ -1,19 +1,22 @@
 """ Utilities module for date manipulation """
 
 from datetime import date, timedelta
+import calendar
 
 
 def date_one_year_from_now() -> date:
     """
     Returns the date exactly one year from today.
 
-    This function calculates a date that is 365 days from the current date,
+    This function calculates the end of the month date from the date that is 365 days from the current date,
     which can be used as a default value for date fields in Django models.
 
     Returns:
-        date: A date object representing one year from today.
+        one_year_later: A date object representing the end of the month one year from today.
     """
-    return date.today() + timedelta(days=365)
+    one_year_later = date.today() + timedelta(days=365)
+    one_year_later = one_year_later.replace(day=calendar.monthrange(one_year_later.year,one_year_later.month)[1])
+    return one_year_later
 
 
 def date_today() -> date:
