a516929b69a62b09da7c1d1748679b060d6fdc0e
ijf <i.filipovic.hr@gmail.com>
2024-11-03 22:08:30 +0100

    expiration_dashboard_v1 feat: Expiration dashboard
    Create day range filtering function.
    Create Proxy model.
    Create custom ModelAdmin.

--- collector/admin.py
+++ collector/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from collector.models import CollectorData
+from collector.models import CollectorData, ExpiringSoonCollectorData
 from django.contrib.auth.models import Group
+from collector.utils.date_utils import days_from_now
+
+admin.site.unregister(Group)
+
+
+class ExpiringSoonCollectorDataAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "expiration_date", "status")
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(expiration_date__lte=days_from_now(14))
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
 
 admin.site.register(CollectorData)
-admin.site.unregister(Group)
+admin.site.register(ExpiringSoonCollectorData, ExpiringSoonCollectorDataAdmin)
