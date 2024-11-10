3708a8a39bb0981d0932344acb5a8696c3a92ab2
ijf <i.filipovic.hr@gmail.com>
2024-11-10 22:52:34 +0100

    default_expiration_date feat: General improvements
    Truncate function name.
    Add date aware mechanism for expiration date.
    Use absolute imports in models.py
    Add migration directory ignore rule for mypy precommit.

--- collector/utils/date_utils.py
+++ collector/utils/date_utils.py
@@ -5,25 +5,30 @@ from django.utils import timezone
 import calendar
 
 
-def calculate_one_year_end_of_month() -> date:
+def one_year_end_of_month() -> date:
     """
     Calculate the last day of the month that occurs one year from the current date.
 
-    This function calculates the end of the month date 
+    This function calculates the end of the month date
     from the date that is 365 days from the current date,
     which can be used as a default value for date fields in Django models.
 
     Returns:
         one_year_later: A `date` object representing the last day of the month
-        one year from today.
+        one year from today, and timezone-aware.
     """
     target_date = date.today() + timedelta(days=365)
     one_year_later = target_date.replace(
-        day=calendar.monthrange(
-            target_date.year,target_date.month
-        )[1]
+        day=calendar.monthrange(target_date.year, target_date.month)[1]
     )
-    return one_year_later
+
+    # Convert the resulting date to a timezone-aware datetime at midnight
+    one_year_later_datetime = timezone.make_aware(
+        timezone.datetime.combine(one_year_later, timezone.datetime.min.time())
+    )
+
+    # Return the timezone-aware datetime as the date (no time part)
+    return one_year_later_datetime.date()
 
 
 def date_today() -> date:
